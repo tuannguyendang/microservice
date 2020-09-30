ff59f6155879609deca24e18e78f1c602ed232b9
tuannguyen <tuan193@gmail.com>
2020-09-30 11:53:14 +0800

    remove skeleton

--- auth/src/main/java/com/dangtuan/auth/util/constants/EntityConstants.java
+++
@@ -1,9 +0,0 @@
-package com.dangtuan.auth.util.constants;
-
-public class EntityConstants {
-
-  public static final String USER_NAME = "username";
-  public static final String AUTHORITIES_TABLE = "authorities";
-  public static final String PASSWORD = "password";
-  public static final String USERS_TABLE = "users";
-}
