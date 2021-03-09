43f8823cb8941ef97e3d67d83d9c0c8360eebaaf
nguyendangtuan <tuan193@gmail.com>
2021-03-09 22:49:00 +0800

    - change test port different with default port

--- order-service/src/integration/scripts/integrationDBSetup.sh
+++ order-service/src/integration/scripts/integrationDBSetup.sh
@@ -1,5 +1,5 @@
 docker kill my_order_test || true;
 sleep 3
-docker run --rm --name my_order_test -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 -e MYSQL_DATABASE=order_db -e MYSQL_USER=order -e MYSQL_PASSWORD=123456 mysql:8.0.21
+docker run --rm --name my_order_test -d -p 3307:3306 -e MYSQL_ROOT_PASSWORD=123456 -e MYSQL_DATABASE=order_db -e MYSQL_USER=order -e MYSQL_PASSWORD=123456 mysql:8.0.21
 sleep 10
 echo "Done setting order database"
